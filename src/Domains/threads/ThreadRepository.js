87449e2601d9848b4d3e6c89187bdbf7d61ab31a
Muhammad Syaoki Faradisa <syaokifaradisa09@gmail.com>
2024-01-16 18:30:19 +0800

    Create ThreadRepository with test

---
+++ src/Domains/threads/ThreadRepository.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
+
+class ThreadRepository {
+  async addThread(addThread) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ThreadRepository;
---
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+
+const ThreadRepository = require('../ThreadRepository');
+
+describe('ThreadRepository interface', () => {
+  it('should throw error when invoke abstract behavior', async () => {
+    const teritoryRepository = new ThreadRepository();
+
+    await expect(teritoryRepository.addThread({})).rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
