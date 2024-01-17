952aae9f233e7ace75c226ca545d3dcfa001f479
Muhammad Syaoki Faradisa <syaokifaradisa09@gmail.com>
2024-01-17 10:00:34 +0800

    Create threads table with test helper

---
+++ migrations/1705454315541_create-table-threads.js
@@ -0,0 +1,28 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    body: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('threads', 'fk_threads_owner_users_id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('threads');
+};
---
+++ tests/ThreadTableTestHelper.js
@@ -0,0 +1,26 @@
+/* eslint-disable linebreak-style */
+/* istanbul ignore file */
+
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const ThreadTableTestHelper = {
+  async addThread({
+    id = 'thread-123',
+    title = 'abc',
+    body = 'abcdef',
+    owner = 'user-123',
+  }) {
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4)',
+      values: [id, title, body, owner],
+    };
+
+    await pool.query(query);
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM threads WHERE 1=1');
+  },
+};
+
+module.exports = ThreadTableTestHelper;
