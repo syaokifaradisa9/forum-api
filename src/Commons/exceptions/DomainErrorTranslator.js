e79429d04a8796cbb0ab498d438ddeea8bb972dc
Muhammad Syaoki Faradisa <syaokifaradisa09@gmail.com>
2024-01-17 21:34:05 +0800

    Create NewComment entities with test

--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -19,6 +19,8 @@ DomainErrorTranslator._directories = {
   'DELETE_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('refresh token harus string'),
   'NEW_THREAD.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('harus mengirimkan title dan body'),
   'NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat thread baru karena tipe data tidak sesuai'),
+  'NEW_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('harus mengirimkan content, threadId dan owner'),
+  'NEW_COMMENT.NOT_MEET_DATA_TYPE_SPESIFICATION': new InvariantError('tidak dapat membuat thread baru karena tipe data tidak sesuai'),
 };
 
 module.exports = DomainErrorTranslator;
---
+++ src/Domains/comments/entities/_test/NewComment.test.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+
+const NewComment = require('../NewComment');
+
+describe('a NewCommentEntities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    const payload = {
+      content: '123',
+    };
+
+    expect(() => new NewComment(payload)).toThrowError('NEW_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type spesification', () => {
+    const payload = {
+      content: 'abc',
+      threadId: 123,
+      owner: [],
+    };
+
+    expect(() => new NewComment(payload)).toThrowError('NEW_COMMENT.NOT_MEET_DATA_TYPE_SPESIFICATION');
+  });
+
+  it('should create newComment object correctly', () => {
+    const payload = {
+      content: 'abcdefg',
+      threadId: 'thread-123',
+      owner: 'user-123',
+    };
+
+    const { content, threadId, owner } = new NewComment(payload);
+
+    expect(content).toEqual(payload.content);
+    expect(threadId).toEqual(payload.threadId);
+    expect(owner).toEqual(payload.owner);
+  });
+});
