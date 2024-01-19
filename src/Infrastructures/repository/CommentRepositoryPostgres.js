01878a130a3e4dee7c78e940ede5a9615a9215a4
Muhammad Syaoki Faradisa <syaokifaradisa09@gmail.com>
2024-01-19 16:27:44 +0800

    add insert date in insert comment and thread at repository

--- src/Infrastructures/repository/CommentRepositoryPostgres.js
+++ src/Infrastructures/repository/CommentRepositoryPostgres.js
@@ -1,5 +1,6 @@
 const AddedComment = require('../../Domains/comments/entities/AddedComment');
 const CommentRepository = require('../../Domains/comments/CommentRepository');
+const Comment = require('../../Domains/comments/entities/Comment');
 
 class CommentRepositoryPostgres extends CommentRepository {
   constructor(pool, idGenerator) {
@@ -12,14 +13,31 @@ class CommentRepositoryPostgres extends CommentRepository {
   async addComment(newComment) {
     const { content, threadId, owner } = newComment;
     const id = `comment-${this.idGenerator()}`;
+    const date = new Date().toISOString();
 
     const query = {
-      text: 'INSERT INTO comments VALUES($1, $2, $3, $4) RETURNING id, content, owner',
-      values: [id, content, threadId, owner],
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5) RETURNING id, content, owner',
+      values: [id, content, threadId, date, owner],
     };
 
     const result = await this.pool.query(query);
     return new AddedComment({ ...result.rows[0] });
+  }
+
+  async getCommentsByThreadId(id) {
+    const query = {
+      text: `
+        SELECT comments.id, comments.content, comments.date, comments.username, users.username
+        FROM comments INNER JOIN users ON comments.owner = users.id
+        WHERE comments.thread_id = $1,
+      `,
+      values: [id],
+    };
+
+    const result = await this.pool.query(query);
+    return result.rows.map((comment) => new Comment({
+      ...comment,
+    }));
   }
 }
 
--- src/Infrastructures/repository/ThreadRepositoryPostgres.js
+++ src/Infrastructures/repository/ThreadRepositoryPostgres.js
@@ -1,5 +1,6 @@
 const ThreadRepository = require('../../Domains/threads/ThreadRepository');
 const AddedThread = require('../../Domains/threads/entities/AddedThread');
+const NotFoundError = require('../../Commons/exceptions/NotFoundError');
 
 class ThreadRepositoryPostgres extends ThreadRepository {
   constructor(pool, idGenerator) {
@@ -12,14 +13,34 @@ class ThreadRepositoryPostgres extends ThreadRepository {
   async addThread(newThread) {
     const { title, body, owner } = newThread;
     const id = `thread-${this.idGenerator()}`;
+    const date = new Date().toISOString();
 
     const query = {
-      text: 'INSERT INTO threads VALUES($1, $2, $3, $4) RETURNING id, title, owner',
-      values: [id, title, body, owner],
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4, $5) RETURNING id, title, owner',
+      values: [id, title, body, date, owner],
     };
 
     const result = await this.pool.query(query);
     return new AddedThread({ ...result.rows[0] });
+  }
+
+  async getThreadById(id) {
+    const query = {
+      text: `
+        SELECT threads.id, threads.title, threads.date, threads.body, users.username,
+        FROM threads
+        INNER JOIN users ON threads.owner = users.id
+        WHERE threads.id = $1
+      `,
+      values: [id],
+    };
+
+    const result = await this.pool.query(query);
+    if (!result.rowCount) {
+      throw new NotFoundError('Data Thread tidak ditemukan');
+    }
+
+    return result.row[0];
   }
 }
 
