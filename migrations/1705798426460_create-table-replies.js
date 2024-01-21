c1ad54ac65e5740e7f31476f6d8c47d87abc67eb
Muhammad Syaoki Faradisa <syaokifaradisa09@gmail.com>
2024-01-21 09:04:42 +0800

    create replies table migration

---
+++ migrations/1705798426460_create-table-replies.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('replies', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    comment_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    isdelete: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: false,
+    },
+  });
+
+  pgm.addConstraint('replies', 'fk_replies_owner_users_id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+  pgm.addConstraint('replies', 'fk_replies_comment_id_comments_id', 'FOREIGN KEY(comment_id) REFERENCES comments(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('comments');
+};
